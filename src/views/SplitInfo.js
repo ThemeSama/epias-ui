1307c8eeefacbf48dc9f10c05b992983772a0007
themesama <themesama9@gmail.com>
2023-02-03 09:52:33 +0300

    Ekran bölme bileşeni bilgi ekranı stilleri düzenlendi

--- src/views/SplitInfo.js
+++ src/views/SplitInfo.js
@@ -9,26 +9,27 @@ const SplitInfo = () => {
   }, []);
 
   return <div className='epias-split-info p-2 d-none d-lg-flex'>
-    <div className='card w-auto ms-auto'>
-      <div className='card-header'>
-        Ayarlar
+    <div className='ms-auto'>
+      <div className='card w-auto'>
+        <div className='card-header'>
+          Ayarlar
+        </div>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            <h6 className='card-title'>Yatay Pencere Değerleri</h6>
+            <p className='card-text'>{humanReadable(sizesState['h'])}</p>
+          </li>
+          <li className='list-group-item'>
+            <h6 className='card-title'>Üst Dikey Pencere Değerleri</h6>
+            <p className='card-text'>{humanReadable(sizesState['vt'])}</p>
+          </li>
+          <li className='list-group-item'>
+            <h6 className='card-title'>Alt Dikey Pencere Değerleri</h6>
+            <p className='card-text'>{humanReadable(sizesState['vb'])}</p>
+          </li>
+        </ul>
       </div>
-      <ul className='list-group list-group-flush'>
-        <li className='list-group-item'>
-          <h6 className='card-title'>Yatay Pencere Değerleri</h6>
-          <p className='card-text'>{humanReadable(sizesState['h'])}</p>
-        </li>
-        <li className='list-group-item'>
-          <h6 className='card-title'>Üst Dikey Pencere Değerleri</h6>
-          <p className='card-text'>{humanReadable(sizesState['vt'])}</p>
-        </li>
-        <li className='list-group-item'>
-          <h6 className='card-title'>Alt Dikey Pencere Değerleri</h6>
-          <p className='card-text'>{humanReadable(sizesState['vb'])}</p>
-        </li>
-      </ul>
     </div>
-    <div className='clearfix' />
   </div>;
 }
 
